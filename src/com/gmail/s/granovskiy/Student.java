754e627d450c9a88b922d72b59b23a6ea502a215
SviatoslavExpert <s.granovskiy@gmail.com>
2018-09-25 08:29:29 +0300

    Initial commit

---
+++ src/com/gmail/s/granovskiy/Student.java
@@ -0,0 +1,43 @@
+package com.gmail.s.granovskiy;
+
+public class Student {
+	private String name;
+	private String lastname;
+	private int course;
+
+	public Student(String name, String lastname, int course) {
+		super();
+		this.name = name;
+		this.lastname = lastname;
+		this.course = course;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getCourse() {
+		return course;
+	}
+
+	public void setCourse(int course) {
+		this.course = course;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", lastname=" + lastname + ", course=" + course + "]";
+	}
+}
